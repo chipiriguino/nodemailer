89e70a891db92636799a4fa2a925a32b76333265
joseBlascoOrtiz <96427738+joseBlascoOrtiz@users.noreply.github.com>
2022-01-27 15:45:50 +0100

    first commit

--- index.js
+++ index.js
@@ -45,6 +45,12 @@ app.post("/api/form",(req,res)=>{
         });
     });
 });
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("build"));
+    app.get("*", (req, res) => {
+      res.sendFile(path.resolve(__dirname,  "build", "index.html"));
+    });
+  }
 
 const PORT=process.env.PORT || 3001;
 
